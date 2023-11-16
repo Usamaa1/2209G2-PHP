acde9f83aa6417b66210e89f8116bb1bfa014b74
Usamaa1 <43386888+Usamaa1@users.noreply.github.com>
2023-11-16 21:00:57 +0500

    View and Insert completed

---
+++ AJAX/query/viewQuery.php
@@ -0,0 +1,28 @@
+<?php include '../connection.php' ?>
+
+<?php
+
+
+$view_query = "SELECT * FROM `ajax`";
+$view_prepare = $connection->prepare($view_query);
+$view_prepare->execute();
+
+$prod_data = $view_prepare->fetchAll(PDO::FETCH_ASSOC);
+
+
+foreach ($prod_data as $prod) {
+
+?>
+
+<div class="card" style="width: 18rem;">
+    <div class="card-body">
+        <h5 class="card-title"><?= $prod['prod_name'] ?></h5>
+        <h6 class="card-subtitle mb-2 text-body-secondary"><?= $prod['prod_price'] ?></h6>
+        <p class="card-text"><?= $prod['prod_description'] ?></p>
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+    </div>
+</div>
+
+
+<?php } ?>
